84b53478cb15320ff444d402a4104dd6242c3997
Aaryan Giradkar <aaryang0605@gmail.com>
2024-04-06 00:00:44 +0530

    Create Sign Up API route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json()); //Allows JSON as the input for our backend
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
