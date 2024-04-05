004da1cd50607bf336e60e59f0c281e45aa497d4
Aaryan Giradkar <aaryang0605@gmail.com>
2024-04-05 22:45:50 +0530

    5. MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connection successful.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
