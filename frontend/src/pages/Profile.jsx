51adef6688d9fc495183f8ba5045339823dd8fa5
Aaryan Giradkar <aaryang0605@gmail.com>
2024-04-08 18:08:41 +0530

    18. Profile page UI

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-5">
+        <img
+          src={currentUser.profilePicture}
+          alt={currentUser.username}
+          className="h-24 w-24 rounded-full object-cover self-center cursor-pointer mt-2"
+        ></img>
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 rounded-lg p-3"
+        ></input>
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 rounded-lg p-3"
+        ></input>
+        <input
+          defaultValue="......."
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 rounded-lg p-3"
+        ></input>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+        <button className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign out
+        </button>
+      </form>
+    </div>
+  );
 }
