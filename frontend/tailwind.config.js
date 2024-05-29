0b04c7700a3fa9121a8370d9a0c591e5dfa70ed2
Aaryan Giradkar <aaryang0605@gmail.com>
2024-05-29 16:26:27 +0530

    34. Tailwind fixed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-
-
-
-
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
-  content: [    
-      "./src/**/*.{html,js}",
-      "./node_modules/tw-elements/js/**/*.js",
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -29,4 +12,4 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
+};
