12afefd493698f1d0791b7e308aa0c54e7704b46
Amol <amolkudale8559@gmail.com>
2024-06-03 17:27:50 +0530

    changes in main

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,19 +14,16 @@
 //   },
 //   plugins: [],
 // };
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    // "./node_modules/tw-elements/dist/js/**/*.js",
-=======
+
+
+
 const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
   content: [    
       "./src/**/*.{html,js,jsx,ts,tsx}",
->>>>>>> 138db0923d26206ee41a11b754f7c8ec7bd41208
+
   ],
   theme: {
     extend: {
